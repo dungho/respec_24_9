7bf09d12b3f0b123357225289b1b2feac648c097
dungho <dung.hoduy@asnet.com.vn>
2012-09-24 10:45:43 +0700

    init code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Respec24Ss9::Application.config.secret_token = '25e5338d29bb3f7b30c435d68e379fb8e89b0dd700048300fe69fdf76c0dec5e4100d7e3b5860f0642834b4656058c5fb84633ef480fdbd6f51e44720002166d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Respec24Ss9::Application.config.session_store :cookie_store, key: '_respec_24_ss9_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Respec24Ss9::Application.config.session_store :active_record_store
